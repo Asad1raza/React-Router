2b0c8cb38c4ee22ea6f321ce23cfc034282a7d5a
Asad1raza <asraza311@gmail.com>
2025-09-26 09:24:32 +0500

    fixed

--- vite-project/src/Dashboard.jsx
+++ vite-project/src/Dashboard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
   function handleClick() {
     navigate("/ ");
